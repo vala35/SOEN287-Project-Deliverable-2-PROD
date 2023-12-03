1bdcd7e7599a76a59656f8f578185d431e45c106
Paul Hatzievagelou <45100450+PaulHatz@users.noreply.github.com>
2023-12-03 18:14:29 -0500

    logs file

---
+++ log.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+var readLogFile = () => {
+    fs.readFile("log.txt", (err, data) => {
+        if (err) throw err;
+        return data;
+    });
+};
+
+var log = (text) => {
+
+    fs.appendFile("log.txt", text, (err) => {
+        if (err) throw err;
+    });
+};
+
+module.exports = { readLogFile, log };
